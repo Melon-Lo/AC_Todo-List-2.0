6ed5b588a8a2f2efd43abd56e63d60b8ca6b2cfc
Melon Lo <dp98062@gmail.com>
2023-06-04 15:15:32 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+
+// 僅在非正式環境時，使用dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// set connection with mongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) 
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongdb error!')
+})
+
+db.once('open', () => { // only happen once
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
